7c360b373cbc2c34e914e4354d4c449f538f1d8c
aviflombaum <avi@flombaum.com>
2012-07-02 11:25:57 -0400

    Generated new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MixtapeappGaFinal::Application.config.secret_token = '271731f88289094040efe68450ae6d6b1f4f8ce63592ff90275cfd2ae3936db54f0adee749b53f2aad2a75e5b5f91494a2561a040e3041221f69544861d40466'
