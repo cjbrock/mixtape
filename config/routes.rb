d46c636deb27fe3afd6312ca02819600bb44f9a6
aviflombaum <avi@flombaum.com>
2012-07-05 12:33:58 -0400

    Set root of site to point to site#home. removed index.html from public

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 MixtapeappGaFinal::Application.routes.draw do
-  get "site/home"
-
   get "/about" => 'site#about'
 
   resources :mixtapes
@@ -56,7 +54,7 @@ MixtapeappGaFinal::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'site#home'
 
   # See how all your routes lay out with "rake routes"
 
